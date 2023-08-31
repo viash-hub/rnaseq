930030e49bdc7e2999b2939f017d61d7d9ef6623
Toni Verbeiren <toni.verbeiren@gmail.com>
2023-08-31 14:27:02 +0200

    Update fastqc component

--- src/qc/fastqc/script.sh
+++ src/qc/fastqc/script.sh
@@ -4,12 +4,4 @@ set -eo pipefail
 
 mkdir -p $par_output 
 
-if [ -f "$par_input_r2" ]; then
-    # paired-end data
-    eval fastqc $par_input $par_input_r2 -o $par_output
-else
-    # single-end data
-    eval fastqc $par_input -o $par_output
-fi
-
-
+fastqc $par_input -o $par_output
