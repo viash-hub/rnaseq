2c2a5e9cd486caf1a34f66f3de269c645fe95401
Toni Verbeiren <toni.verbeiren@gmail.com>
2023-08-31 15:14:45 +0200

    fastqc component explicitly takes a paired argument

--- src/qc/fastqc/script.sh
+++ src/qc/fastqc/script.sh
@@ -4,4 +4,22 @@ set -eo pipefail
 
 mkdir -p $par_output 
 
-fastqc $par_input -o $par_output
+IFS="," read -ra input <<< "$par_input"
+
+count="${#input[@]}"
+
+if [ "$par_paired" == "true" ]; then
+  echo "Paired - $count"
+  if [ "$count" -ne 2 ]; then
+    echo "Paired end input requires two files"
+    exit 1
+  fi
+else
+  echo "Not Paired - $count"
+  if [ "$count" -ne 1 ]; then
+    echo "Single end input requires one file"
+    exit 1
+  fi
+fi
+
+eval fastqc "${input[*]}" -o $par_output
