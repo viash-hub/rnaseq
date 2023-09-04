57483a10839322ccafd669864d2c46bbfcb07b0a
Nirmayi <ynirmayi@gmail.com>
2023-09-04 11:16:09 +0200

    added extra argument to untar command

--- src/untar/script.sh
+++ src/untar/script.sh
@@ -9,9 +9,7 @@ mkdir -p $prefix
 ## Ensures --strip-components only applied when top level of tar contents is a directory
 ## If just files or multiple directories, place all in prefix
 if [[ \$(tar -taf ${par_input} | grep -o -P "^.*?\\/" | uniq | wc -l) -eq 1 ]]; then
-    tar -C $prefix --strip-components 1 -xavf $par_input \
-        # args $args2
+    tar -C $prefix --strip-components 1 -xavf $par_input --no-same-owner
 else
-    tar -C $prefix -xavf $par_input \
-        # $args $args2
+    tar -C $prefix -xavf $par_input --no-same-owner
 fi
