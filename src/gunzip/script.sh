65e405f35a111443ae3cc3246855820a5fc27f7c
Nirmayi <ynirmayi@gmail.com>
2023-09-04 10:33:58 +0200

    gunzip component

--- src/gunzip/script.sh
+++ src/gunzip/script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 set -eo pipefail
-par_input="/home/nirmayi/data_intuitive/rnaseq.vsh/testData/reference/genes.gff.gz"
-
-par_output=(echo "${par_input%%.gz}")
 
 gunzip -f $par_input 
