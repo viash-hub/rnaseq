0525952e04a36dedecc6f918c6923adca38400b8
Nirmayi <ynirmayi@gmail.com>
2023-09-04 09:13:19 +0200

    Get reference data

---
+++ bin/get_reference.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CURR=`pwd`
+DEST="testData/reference/"
+mkdir -p $DEST
+cd $DEST
+
+echo "Fetching test data from https://github.com/nf-core/test-datasets/tree/rnaseq3/reference..."
+
+wget https://github.com/nf-core/test-datasets/blob/rnaseq3/reference/bbsplit_fasta_list.txt
+wget https://github.com/nf-core/test-datasets/blob/rnaseq3/reference/genes.gff.gz
+wget https://github.com/nf-core/test-datasets/blob/rnaseq3/reference/genes.gtf.gz
+wget https://github.com/nf-core/test-datasets/blob/rnaseq3/reference/genome.fasta
+wget https://github.com/nf-core/test-datasets/blob/rnaseq3/reference/gfp.fa.gz
+wget https://github.com/nf-core/test-datasets/blob/rnaseq3/reference/hisat2.tar.gz
+wget https://github.com/nf-core/test-datasets/blob/rnaseq3/reference/rsem.tar.gz
+wget https://github.com/nf-core/test-datasets/blob/rnaseq3/reference/salmon.tar.gz
+wget https://github.com/nf-core/test-datasets/blob/rnaseq3/reference/transcriptome.fasta
+
+cd $CURR
