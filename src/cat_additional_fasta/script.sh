237d9e6ffb990ce77f6a1cd40f618992af0c0b58
Nirmayi <ynirmayi@gmail.com>
2023-09-05 09:00:59 +0200

    Component to concatenate additional fasta and gtf to reference

---
+++ src/cat_additional_fasta/script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+genome_name="${par_fasta##*/}"
+genome_name="${genome_name%%.*}"
+
+biotype_name=''
+if [ biotype ]; then 
+    biotype_name="-b $par_biotype"
+fi
+
+add_name="${par_additional_fasta##*/}"
+add_name="${par_additional_fasta%%.*}"
+
+name="${genome_name}_${add_name}"
+
+# Use fasta2gtf.py to generate a GTF annotation file from the FASTA file
+fasta2gtf.py -o "$add_name.gtf" $biotype_name $par_additional_fasta
+
+cat $par_fasta $par_additional_fasta > $par_fasta_output
+cat $par_gtf "$add_name.gtf" > $par_gtf_output
