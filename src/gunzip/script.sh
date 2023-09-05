9d98ce1f22df6ad8bd1f53b7316c6acbefe18a97
Nirmayi <ynirmayi@gmail.com>
2023-09-05 18:43:43 +0200

    updated output in script

--- src/gunzip/script.sh
+++ src/gunzip/script.sh
@@ -2,4 +2,5 @@
 
 set -eo pipefail
 
+par_output="${par_input%%.gz}"
 gunzip -f $par_input 
--- src/untar/script.sh
+++ src/untar/script.sh
@@ -2,16 +2,14 @@
 
 set -eo pipefail
 
-# par_input="/home/nirmayi/data_intuitive/rnaseq.vsh/testData/reference/hisat2.tar.gz"
+par_output=`echo "${par_input%%.tar.gz}"`
 
-prefix=`echo "${par_input%%.tar.gz}"`
-
-mkdir -p $prefix
+mkdir -p $par_output
 
 ## Ensures --strip-components only applied when top level of tar contents is a directory
 ## If just files or multiple directories, place all in prefix
 if [[ $(tar -taf ${par_input} | grep -o -P "^.*?\\/" | uniq | wc -l) -eq 1 ]]; then
-    tar -C $prefix --strip-components 1 -xavf $par_input --no-same-owner
+    tar -C $par_output --strip-components 1 -xavf $par_input --no-same-owner
 else
-    tar -C $prefix -xavf $par_input --no-same-owner
+    tar -C $par_output -xavf $par_input --no-same-owner
 fi
