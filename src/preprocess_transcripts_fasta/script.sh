4767e3dac6d080206a47c5189ae5026cde42b20c
Nirmayi <ynirmayi@gmail.com>
2023-09-19 10:37:42 +0200

    changed output name

--- src/preprocess_transcripts_fasta/script.sh
+++ src/preprocess_transcripts_fasta/script.sh
@@ -7,4 +7,4 @@ if [ "${par_transcript_fasta##*.}" == ".gz" ]; then
     command="zcat"
 fi
 
-$command $par_transcript_fasta | cut -d "|" -f1 > $par_fixed_fasta
+$command $par_transcript_fasta | cut -d "|" -f1 > $par_output
