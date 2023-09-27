acdcc3e3d8f0df8605d5a69ca91b95955d5c7da5
Nirmayi <ynirmayi@gmail.com>
2023-09-27 09:56:58 +0200

    fixed errors

--- src/bbmap_bbsplit/script.sh
+++ src/bbmap_bbsplit/script.sh
@@ -18,27 +18,24 @@ if $par_only_build_index; then
         echo "ERROR: Please specify as input a primary fasta file along with names and paths to non-primary fasta files."
     fi
 else
-    # IFS="," read -ra input <<< "$par_input"
+    mkdir -p $par_filtered_output
     index_files=''
-    if [ -e "$par_bbsplit_index" ]; then
-        index_files="path=$par_bbsplit_index"
+    if [ -d "$par_built_bbsplit_index" ]; then
+    index_files="path=$par_built_bbsplit_index"
     elif [ -f "$par_primary_ref" ] && [ ${#other_refs[@]} -gt 0 ]; then
         index_files="ref_primary=$primary_ref ${other_refs[@]}"
     else
         echo "ERROR: Please either specify a BBSplit index as input or a primary fasta file along with names and paths to non-primary fasta files."
     fi
-    # fastq_in=`$par_paired ? "in=${input[0]} in2=${input[1]}" : "in=${input}"`
-    # fastq_out=`$par_paired ? "basename=${par_id}_%_#.fastq.gz" : "basename=${par_id}_%.fastq.gz"`
-    read1=$(find $par_input -iname *_1_trimmed.fq.gz*)
-    read2=$(find $par_input -iname *_2_trimmed.fq.gz*)
     if $par_paired; then
+        read1=$(find $par_input/ -iname *_1*.fq.gz*)
+        read2=$(find $par_input/ -iname *_2*.fq.gz*)
         fastq_in="in=$read1 in2=$read2"
-        fastq_out="basename=${par_id}_%_#.fastq.gz"
+        fastq_out="basename=${par_filtered_output}/%_#.fastq.gz"
     else
+        read1=$(find $par_input/ -iname *trimmed.fq.gz*)
         fastq_in="in=$read1"
-        fastq_out="basename=${par_id}_%.fastq.gz"
+        fastq_out="basename=${par_filtered_output}/%.fastq.gz"
     fi
-
-    bbsplit.sh -Xmx${avail_mem}M $index_files threads=$meta_cpus $fastq_in $fastq_out refstats=${par_id}.stats.txt
+    bbsplit.sh -Xmx${avail_mem}M $index_files threads=$meta_cpus $fastq_in $fastq_out refstats=bbsplit_stats.txt
 fi
-cp "${par_id}_primary.fastq.gz" $par_filtered_output
