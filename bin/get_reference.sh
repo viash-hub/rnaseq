02ca743d176952ce1aaf1bc2ed3816b0bcda8a89
Nirmayi <ynirmayi@gmail.com>
2023-10-12 14:15:19 +0200

    Script to fetch all the reference data files

--- bin/get_reference.sh
+++ bin/get_reference.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CURR=`pwd`
-DEST="testData/reference/"
+DEST="testData/reference"
 mkdir -p $DEST
 cd $DEST
 
@@ -19,3 +19,30 @@ wget https://raw.githubusercontent.com/nf-core/test-datasets/rnaseq3/reference/t
 wget https://raw.githubusercontent.com/nf-core/rnaseq/3.12.0/assets/rrna-db-defaults.txt
 
 cd $CURR
+
+NEWDEST1="testData/reference/rRNA"
+mkdir -p $NEWDEST1
+cd $NEWDEST1
+for LINE in `cat ../rrna-db-defaults.txt`
+do
+    wget $LINE
+done
+
+cd $CURR
+
+find $NEWDEST1 -type f > $DEST/rrna-db-defaults.txt
+
+NEWDEST2="testData/reference/bbsplit_fasta"
+mkdir -p $NEWDEST2
+
+while IFS=, read -r -a line; do
+    url="${line[1]}"
+    name="$NEWDEST2/${line[0]}.fa"
+    wget $url -O "$name"
+    line+=("$name")
+    IFS=','
+    echo "${line[*]}" >> "$NEWDEST2/tmp.txt"
+done < "$DEST/bbsplit_fasta_list.txt"
+
+cut -d',' -f1,3 "$NEWDEST2/tmp.txt" > "$DEST/bbsplit_fasta_list.txt"
+rm "$NEWDEST2/tmp.txt"
