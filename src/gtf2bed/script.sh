ae34df61fe5330eec730ec596f78356c174f42ee
dorien-er <roosen.dorien@gmail.com>
2023-10-30 16:13:06 +0100

    adjust viash config file to fix docker bug

--- src/gtf2bed/script.sh
+++ src/gtf2bed/script.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
 
 set -eo pipefail 
 
 filename="$(basename -- $par_gtf/*.gtf)"
 mkdir -p $par_bed_output
 
-gtf2bed $par_gtf/$filename > $par_bed_output/${filename%%.*}.bed
+gtf2bed < $par_gtf/$filename > $par_bed_output/${filename%%.*}.bed
