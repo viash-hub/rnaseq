d644837a179352d1b6be8e2dca91d320d45eef7e
Nirmayi <ynirmayi@gmail.com>
2023-10-31 16:49:06 +0100

    Check if tar archive

--- src/untar/script.sh
+++ src/untar/script.sh
@@ -6,8 +6,12 @@ mkdir -p $par_output
 
 ## Ensures --strip-components only applied when top level of tar contents is a directory
 ## If just files or multiple directories, place all in prefix
-if [[ $(tar -taf $par_input | grep -o -P "^.*?\\/" | uniq | wc -l) -eq 1 ]]; then
-    tar -C $par_output --strip-components 1 -xavf $par_input --no-same-owner
+if [ ${par_input#*.} == "tar.gz" ]; then
+    if [[ $(tar -taf $par_input | grep -o -P "^.*?\\/" | uniq | wc -l) -eq 1 ]]; then
+        tar -C $par_output --strip-components 1 -xavf $par_input --no-same-owner
+    else
+        tar -C $par_output -xavf $par_input --no-same-owner
+    fi
 else
-    tar -C $par_output -xavf $par_input --no-same-owner
+    cat $par_input > $par_output
 fi
