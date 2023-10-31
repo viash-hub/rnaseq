21eb05ca5da55a0a12e39d6bd61d77c822c67933
Nirmayi <ynirmayi@gmail.com>
2023-10-31 11:45:32 +0100

    adjusted component to use gtf2bed Perl script from nf-core/rnaseq/bin

--- src/gtf2bed/script.sh
+++ src/gtf2bed/script.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail 
 
-filename="$(basename -- $par_gtf/*.gtf)"
+filename=$(basename -- $par_gtf/*.gtf)
 mkdir -p $par_bed_output
 
-gtf2bed $par_gtf/$filename > $par_bed_output/${filename%%.*}.bed
+"$meta_resources_dir/gtf2bed" $par_gtf/$filename > $par_bed_output/${filename%%.*}.bed 
