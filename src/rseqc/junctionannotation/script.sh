d8118167627e6bfd6d11055ec93943c73caa3b5a
dorien-er <roosen.dorien@gmail.com>
2023-11-01 12:28:31 +0100

    adjust input vars

--- src/rseqc/junctionannotation/script.sh
+++ src/rseqc/junctionannotation/script.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 set -eo pipefail 
 
 filename="$(basename -- $par_bam_input)"
-ref_file="$(basename -- $par_bed_input)"
+ref_file="$(basename -- $par_refgene)"
 mkdir -p $par_output
 
-junction_annotation.py -i $par_bam_input -r $par_bed_input -o $par_output 2> $par_output/${filename%%.*}.${ref_file%%.*}.junction_annotation.log
+junction_annotation.py -i $par_bam_input -r $par_refgene -o $par_output 2> $par_output/${filename%%.*}.${ref_file%%.*}.junction_annotation.log
