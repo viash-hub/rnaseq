9f67d64120ac63a80bf51d57b091a18a03724b54
dorien-er <roosen.dorien@gmail.com>
2023-11-01 09:51:32 +0100

    update input arguments

--- src/rseqc/bamstat/script.sh
+++ src/rseqc/bamstat/script.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail 
 
-filename="$(basename -- $par_input)"
+filename="$(basename -- $par_bam_input)"
 mkdir -p $par_output
 
-bam_stat.py --input $par_input > $par_output/${filename%%.*}.bamstat.txt
+bam_stat.py --input $par_bam_input > $par_output/${filename%%.*}.bamstat.txt
