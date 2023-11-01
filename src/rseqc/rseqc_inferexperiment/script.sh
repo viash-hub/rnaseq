6aa6c3541841de23d889880ff693afad57a8d43e
dorien-er <roosen.dorien@gmail.com>
2023-11-01 10:50:06 +0100

    adjust input params

--- src/rseqc/rseqc_inferexperiment/script.sh
+++ src/rseqc/rseqc_inferexperiment/script.sh
@@ -3,7 +3,7 @@
 set -eo pipefail 
 
 filename="$(basename -- $par_bam_input)"
-ref_file="$(basename -- $par_bed_input)"
+ref_file="$(basename -- $par_refgene)"
 mkdir -p $par_output
 
-infer_experiment.py -i $par_bam_input -r $par_bed_input > $par_output/${filename%%.*}.${ref_file%%.*}.infer_experiment.txt
+infer_experiment.py -i $par_bam_input -r > $par_output/${filename%%.*}.${ref_file%%.*}.infer_experiment.txt
