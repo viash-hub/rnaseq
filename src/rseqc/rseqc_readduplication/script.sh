47b3607252db83818775aa0abb695538de6a4d01
dorien-er <roosen.dorien@gmail.com>
2023-11-01 14:07:11 +0100

    rseqc read duplication module

--- src/rseqc/rseqc_readduplication/script.sh
+++ src/rseqc/rseqc_readduplication/script.sh
@@ -2,8 +2,6 @@
 
 set -eo pipefail 
 
-filename="$(basename -- $par_bam_input)"
-ref_file="$(basename -- $par_refgene)"
 mkdir -p $par_output
 
-junction_saturation.py -i $par_bam_input -r $par_refgene -o $par_output 
+read_duplication.py -i $par_bam_input -o $par_output 
