7de195fa4c3fb017fc6d6869685b71ba5b1c43e9
dorien-er <roosen.dorien@gmail.com>
2023-11-06 15:35:51 +0100

    viash module for read dupcliation rate and tests

--- src/rseqc/readduplication/script.sh
+++ src/rseqc/readduplication/script.sh
@@ -2,6 +2,15 @@
 
 set -eo pipefail 
 
-mkdir -p $par_output
+filename="$(basename -- $par_input)"
 
-read_duplication.py -i $par_bam_input -o $par_output 
+read_duplication.py \
+    -i $par_input \
+    -o "$filename" \
+    -u $par_read_count_upper_limit \
+    -q $par_map_qual 
+
+mv $filename.DupRate_plot.pdf $par_output_duplication_rate_plot
+mv $filename.DupRate_plot.r $par_output_duplication_rate_plot_r
+mv $filename.pos.DupRate.xls $par_output_duplication_rate_mapping
+mv $filename.seq.DupRate.xls $par_output_duplication_rate_sequence
