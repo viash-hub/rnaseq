b6619497b8e3fda1306fbed3845021782353499f
dorien-er <roosen.dorien@gmail.com>
2023-11-06 09:21:26 +0100

    bamstat test and output update

--- src/rseqc/bamstat/script.sh
+++ src/rseqc/bamstat/script.sh
@@ -3,9 +3,8 @@
 set -eo pipefail 
 
 filename="$(basename -- $par_input)"
-mkdir -p $par_output
 
 bam_stat.py \
     --input $par_input \
     --mapq $par_map_qual \
-> $par_output/${filename%%.*}.bamstat.txt
+> $par_output
--- src/rseqc/bamstat/test.sh
+++ src/rseqc/bamstat/test.sh
@@ -9,14 +9,18 @@ meta_temp_dir="output/bamstat"
 
 sample="SRR6357070"
 
+echo "> Running $meta_functionality_name."
 
 "$meta_executable" \
-    --input "$meta_resources_dir/$sample.bam" \
-    --output "$meta_temp_dir"
+    --input "$meta_resources_dir/test/$sample.bam" \
+    --output "$meta_temp_dir/mapping_quality.txt"
 
+exit_code=$?
+[[ $exit_code != 0 ]] && echo "Non zero exit code: $exit_code" && exit 1
 
-[[ ! -d "$meta_temp_dir" ]] && echo "Output dir could not be found!" && exit 1
+echo ">> Checking whether output can be found and has content"
 
-[[ ! -f "$meta_temp_dir/$sample".bamstat.txt ]] && echo "Bamstat summary file missing" && exit 1
+[[ ! -f "$meta_temp_dir"/mapping_quality.txt ]] && echo "mapping_quality.txt file missing" && exit 1
+[[ -z "$meta_temp_dir"/mapping_quality.txt ]] && echo "Bamstat file is empty" && exit 1
 
 exit 0
