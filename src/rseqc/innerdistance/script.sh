76b4a9e244666ac9c21f1d2137b60f73c01b8c60
dorien-er <roosen.dorien@gmail.com>
2023-11-06 13:47:56 +0100

    make output for inner distance not required but explicit

--- src/rseqc/innerdistance/script.sh
+++ src/rseqc/innerdistance/script.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-set -eo pipefail 
+set -exo pipefail 
 
 filename="$(basename -- $par_input)"
 
 if $par_paired; then
     inner_distance.py -i $par_input -r $par_refgene -o $filename > stdout.txt
     head -n 2 stdout.txt > $filename.inner_distance_stats.txt
+    mv $filename.inner_distance.txt $par_output_dist
+    mv $filename.inner_distance_plot.pdf $par_output_plot
+    mv $filename.inner_distance_plot.r $par_output_plot_r
+    mv $filename.inner_distance_freq.txt $par_output_freq
 fi
