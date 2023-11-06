c5a0ea2589d3a596d223eabb417cc71d243e9395
dorien-er <roosen.dorien@gmail.com>
2023-11-06 14:07:17 +0100

    generate junction annotation module with test

--- src/rseqc/junctionannotation/script.sh
+++ src/rseqc/junctionannotation/script.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 set -eo pipefail 
-set -eo pipefail 
 
-filename="$(basename -- $par_bam_input)"
-ref_file="$(basename -- $par_refgene)"
-mkdir -p $par_output
+filename="$(basename -- $par_input)"
 
-junction_annotation.py -i $par_bam_input -r $par_refgene -o $par_output 2> $par_output/${filename%%.*}.${ref_file%%.*}.junction_annotation.log
+junction_annotation.py \
+    -i $par_input \
+    -r $par_refgene \
+    -o "$filename" \
+2> $par_output_log
+
+mv $filename.junction.bed $par_output_junction_bed
+mv $filename.junction.Interact.bed $par_output_junction_interact
+mv $filename.junction.xls $par_output_junction_sheet
+mv $filename.junction_plot.r $par_output_plot_r
+mv $filename.splice_events.pdf $par_output_splice_events_plot
+mv $filename.splice_junction.pdf $par_output_splice_junctions_plot
---
+++ src/rseqc/junctionannotation/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "> Running $meta_functionality_name."
+
+sample="SRR6357070"
+genome="genome_gfp"
+
+"$meta_executable" \
+    --input "$meta_resources_dir/test/$sample.bam" \
+    --refgene "$meta_resources_dir/test/$genome.bed" \
+
+exit_code=$?
+[[ $exit_code != 0 ]] && echo "Non zero exit code: $exit_code" && exit 1
+
+echo ">> asserting  all output files were created"
+
+[[! -f "$meta_temp_dir/$output_log" ]] && echo "$output_log was not created" && exit 1
+[[! -f "$meta_temp_dir/$par_output_junction_bed" ]] && echo "$par_output_junction_bed was not created" && exit 1
+[[! -f "$meta_temp_dir/$sample.$par_output_junction_interact" ]] && echo "$par_output_junction_interact was not reated" && exit 1
+[[! -f "$meta_temp_dir/$sample.$par_output_junction_sheet" ]] && echo "$par_output_junction_sheet was not created" && exit 1
+[[! -f "$meta_temp_dir/$sample.$par_output_plot_r" ]] && echo "$par_output_plot_r was not created" && exit 1
+[[! -f "$meta_temp_dir/$sample.$par_output_splice_events_plot" ]] && echo "$par_output_splice_events_plot was not created" && exit 1
+[[! -f "$meta_temp_dir/$sample.$par_output_splice_junctions_plot" ]] && echo "$par_output_splice_junctions_plot was not created" && exit 1
+
+
+exit 0
