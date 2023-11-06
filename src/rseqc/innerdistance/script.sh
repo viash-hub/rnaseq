81432e6f379aebafdc7b7177805e5fa60df3a687
dorien-er <roosen.dorien@gmail.com>
2023-11-06 14:59:04 +0100

    add optional params to bash script

--- src/rseqc/innerdistance/script.sh
+++ src/rseqc/innerdistance/script.sh
@@ -5,8 +5,19 @@ set -exo pipefail
 filename="$(basename -- $par_input)"
 
 if $par_paired; then
-    inner_distance.py -i $par_input -r $par_refgene -o $filename > stdout.txt
+    inner_distance.py \
+        -i $par_input \
+        -r $par_refgene \
+        -o $filename \
+        -k $par_sample_size \
+        -l $par_lower_bound_size \
+        -u $par_upper_bound_size \
+        -s $par_step_size \
+        -q $par_map_qual \
+    > stdout.txt
+
     head -n 2 stdout.txt > $filename.inner_distance_stats.txt
+    
     mv $filename.inner_distance.txt $par_output_dist
     mv $filename.inner_distance_plot.pdf $par_output_plot
     mv $filename.inner_distance_plot.r $par_output_plot_r
