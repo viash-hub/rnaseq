fecc20acef9b382a71d61f2cb70d8b3fb51e209f
Nirmayi <ynirmayi@gmail.com>
2023-11-09 13:15:41 +0100

    added multiqc_custom_biotype module

---
+++ src/multiqc_custom_biotype/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eo pipefail
+
+cut -f 1,7 $par_biocounts | tail -n +3 | cat $par_biotypes_header - >> biotype_counts_mqc.tsv
+
+mqc_features_stat.py \
+    biotype_counts_mqc.tsv \
+    -s $par_id \\
+    -f rRNA \\
+    -o $par_featurecounts_multiqc
