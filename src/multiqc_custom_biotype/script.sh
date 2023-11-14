c3d00473e619b90c5cc07ba6d32d7f80e2d4a15a
Nirmayi <ynirmayi@gmail.com>
2023-11-14 09:25:19 +0100

    corrected python version and fixed minor errors

--- src/multiqc_custom_biotype/script.sh
+++ src/multiqc_custom_biotype/script.sh
@@ -4,8 +4,8 @@ set -eo pipefail
 
 cut -f 1,7 $par_biocounts | tail -n +3 | cat $par_biotypes_header - >> biotype_counts_mqc.tsv
 
-mqc_features_stat.py \
+"$meta_resources_dir/mqc_features_stat.py" \
     biotype_counts_mqc.tsv \
-    -s $par_id \\
-    -f rRNA \\
+    -s $par_id \
+    -f rRNA \
     -o $par_featurecounts_multiqc
