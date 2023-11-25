282962bc6fa433a44f4d112fb85c6d0ad76c4807
Nirmayi <ynirmayi@gmail.com>
2023-11-25 15:00:53 +0100

    resource test script guide

---
+++ src/resources_test_scripts/some_fastq.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+OUTPUT_DIR="resources_test/some_fastq"
+
+# all of the commands needed to generate `resources_test/some_fastq`
+
+# we make sure that the name of the script (i.e. src/resources_test_scripts/some_fastq.sh) corresponds to the output dir.
