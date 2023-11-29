760c2e1ece08c77d9a1ca6a0496928343ed59977
Nirmayi <ynirmayi@gmail.com>
2023-11-29 22:15:03 +0100

    added unit test

--- src/cat_additional_fasta/test.sh
+++ src/cat_additional_fasta/test.sh
@@ -5,18 +5,23 @@ meta_resources_dir="..."
 meta_executable="..."
 ## VIASH END
 
+gunzip "$meta_resources_dir/genes.gtf"
+gunzip "$meta_resources_dir/gfp.fa"
+
 "$meta_executable" \
-  --id mysample_id \
-  --paired true \
-  --input "$meta_resources_dir/some_fastq/input_r1.fastq,$meta_resources_dir/some_fastq/input_r2.fastq" \
-  ... other params ... \
-  --bbsplit_index foo \
-  --filtered_output bar
+  --fasta "$meta_resources_dir/genome.fasta" \
+  --gtf "$meta_resources_dir/genes.gtf" \
+  --additional_fasta "$meta_resources_dir/gfp.fa" \
+  --biotype gene_biotype \
+  --fasta_output genome_gfp.fasta \
+  --gtf_output genome_gfp.gtf
+
+exit_code=$?
+[[ $exit_code != 0 ]] && echo "Non zero exit code: $exit_code" && exit 1
 
 # check whether output exists
-[ ! -d foo ] && "Directory 'foo' does not exist!" && exit 1
-[ ! -d bar ] && "Directory 'bar' does not exist!" && exit 1
-
-# TODO: check contents of foo and bar
+[ ! -f genome_gfp.fasta ] && echo "File 'genome_gfp.fasta' does not exist!" && exit 1
+[ ! -f genome_gfp.gtf ] && echo "File 'genome_gfp.gtf' does not exist!" && exit 1
 
 echo "All tests succeeded!"
+exit 0
