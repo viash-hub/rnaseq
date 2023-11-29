3d3038ac6c3df8e5fbd575be45cb4a3cce0a1c48
Nirmayi <ynirmayi@gmail.com>
2023-11-30 00:06:33 +0100

    add unit tests

--- src/bbmap_bbsplit/test.sh
+++ src/bbmap_bbsplit/test.sh
@@ -5,6 +5,8 @@ meta_resources_dir="..."
 meta_executable="..."
 ## VIASH END
 
+echo ">>> Testing with paired-end reads"
+
 "$meta_executable" \
   --id mysample_id \
   --paired true \
@@ -17,6 +19,21 @@ meta_executable="..."
 [ ! -d foo ] && "Directory 'foo' does not exist!" && exit 1
 [ ! -d bar ] && "Directory 'bar' does not exist!" && exit 1
 
-# TODO: check contents of foo and bar
+echo ">>> Testing with single-end reads"
+
+"$meta_executable" \
+  --id mysample_id \
+  --paired true \
+  --input "$meta_resources_dir/some_fastq/input_r1.fastq,$meta_resources_dir/some_fastq/input_r2.fastq" \
+  ... other params ... \
+  --bbsplit_index foo \
+  --filtered_output bar
+
+# check whether output exists
+[ ! -d foo ] && "Directory 'foo' does not exist!" && exit 1
+[ ! -d bar ] && "Directory 'bar' does not exist!" && exit 1
+
+# TODO: check contents of output
 
 echo "All tests succeeded!"
+exit 0
---
+++ src/bedtools_genomecov/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo ">>> Testing $meta_functionality_name"
+
+"$meta_executable" \
+    --id $id \
+    --strandedness unstranded \
+    --bam $id.sorted.bam \
+    --bedgraph_forward $id.forward.bedgraph \
+    --bedgraph_reverse $id.reverse.bedgraph
+
+exit_code=$?
+[[ $exit_code != 0 ]] && echo "Non zero exit code: $exit_code" && exit 1
+
+# check whether output exists
+[ ! -f $id.forward.bedgraph ] && echo "File '$id.forward.bedgraph' does not exist!" && exit 1
+[ ! -f $id.reverse.bedgraph ] && echo "File '$id.reverse.bedgraph' does not exist!" && exit 1
+
+echo "All tests succeeded!"
+exit 0
