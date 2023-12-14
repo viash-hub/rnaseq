8e3a253992f687a7ab24f0cd032d2ae73629d299
Nirmayi <ynirmayi@gmail.com>
2023-12-14 13:23:30 +0100

    use semicolon as multiple separator

--- src/cat_fastq/script.sh
+++ src/cat_fastq/script.sh
@@ -2,5 +2,15 @@
 
 set -eo pipefail
 
-cat $par_read_1 > $par_fastq_1
-cat $par_read_2 > $par_fastq_2
+IFS=";" read -ra read_1 <<< $par_read_1
+IFS=";" read -ra read_2 <<< $par_read_2
+
+# cat $read_1 > $par_fastq_1
+# cat $read_2 > $par_fastq_2
+
+if [ ${#read_1[@]} -gt 0 ]; then
+    cat $read_1 > $par_fastq_1
+fi
+if [ ${#read_2[@]} -gt 0 ]; then
+    cat $read_2 > $par_fastq_2
+fi
