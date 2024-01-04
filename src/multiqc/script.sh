d352d7b08037a6708ea8bda843bf4f6c3b22e65f
Nirmayi <ynirmayi@gmail.com>
2024-01-04 09:39:32 +0100

    add custom config

--- src/multiqc/script.sh
+++ src/multiqc/script.sh
@@ -2,12 +2,10 @@
 
 set -eo pipefail
 
-# multiqc_config="assets/multiqc_config.yml"
-
 multiqc \
     -f -p \
     ${par_multiqc_title:+--title $par_multiqc_title} \
-    ${par_multiqc_custom_config:+--config $par_multiqc_custom_config} \
+    --config $par_multiqc_custom_config \
     $par_input
 
 # cat <<-END_VERSIONS > versions.yml
