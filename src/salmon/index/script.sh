0f69826d2f9002c4aa5dcd46c7ba3539d6af6817
Nirmayi <ynirmayi@gmail.com>
2024-03-19 10:09:05 +0100

    update input parameters

--- src/salmon/index/script.sh
+++ src/salmon/index/script.sh
@@ -18,12 +18,12 @@ salmon index \
     ${meta_cpus:+--threads $meta_cpus} \
     -t $gentrome \
     -i $par_salmon_index
+    ${par_pseudo_aligner_kmer_size:+-k $par_pseudo_aligner_kmer_size} \
+    ${par_gencode:+--gencode}
 
 # Version
-
 text="${meta_functionality_name}:
     salmon: $(echo $(salmon --version) | sed -e 's/salmon //g')"
-
 if [ -e "$par_versions" ]; then
     echo "$text" >> "$par_versions"
     mv "$par_versions" "$par_updated_versions"
