730b43ad07fc6cebd3b8f34f2f8ad13077a543cf
Nirmayi <ynirmayi@gmail.com>
2024-03-29 04:50:09 +0100

    update test

--- src/workflows/pre_processing/test_run.sh
+++ src/workflows/pre_processing/test_run.sh
@@ -2,35 +2,19 @@
 
 viash ns build --parallel --setup cb
 
-# nextflow run target/nextflow/workflows/pre_processing/main.nf \
-#   --id SRR6357070_1 \
-#   --input 'testData/minimal_test/SRR6357070_1.fastq.gz' \
-#   --publish_dir "testData/test_output" \
-#   --umitools_bc_pattern "NNNN" \
-#   # -profile docker \
-#   # -resume
-
-# Test single-end data
-# cat > testData/minimal_test/sample_sheet.csv << HERE
-# id,fastq_1
-# SRR6357070_1,SRR6357070_1.fastq.gz
-# SRR6357071_1,SRR6357071_1.fastq.gz
-# HERE
-
-# nextflow run target/nextflow/workflows/pre_processing/main.nf \
-#   --param_list testData/minimal_test/sample_sheet.csv \
-#   --publish_dir "testData/single_end_test" \
-#   --umitools_bc_pattern "NNNN" \
-#   --bbsplit_fasta_list testData/reference/bbsplit_fasta_list.txt \
-#   --fasta testData/test_output/ref.prepare_genome.fasta_uncompressed \
-#   --bbsplit_index testData/test_output/ref.prepare_genome.bbsplit_index_uncompressed \
-#   --ribo_database_manifest testData/reference/rrna-db-defaults.txt 
+nextflow run target/nextflow/workflows/pre_processing/main.nf \
+  --id RAP1_UNINDUCED_REP1 \
+  --input 'testData/minimal_test/input_fastq/SRR6357073_1.fastq.gz' \
+  --publish_dir "test_results/preprocessing_no_samplesheet" \
+  --umitools_bc_pattern "NNNN" \
+  -profile docker \
+  -resume
 
 # Test paired-end data
 cat > testData/minimal_test/sample_sheet.csv << HERE
-id,fastq_1,fastq_2
-SRR6357070,SRR6357070_1.fastq.gz,SRR6357070_2.fastq.gz
-SRR6357071,SRR6357071_1.fastq.gz,SRR6357071_2.fastq.gz
+id,fastq_1,fastq_2,strandedness
+WT_REP2,SRR6357072_1.fastq.gz,SRR6357072_2.fastq.gz,reverse
+RAP1_IAA_30M_REP1,SRR6357076_1.fastq.gz,SRR6357076_2.fastq.gz,reverse
 HERE
 
 nextflow run target/nextflow/workflows/pre_processing/main.nf \
@@ -38,13 +22,12 @@ nextflow run target/nextflow/workflows/pre_processing/main.nf \
   --publish_dir "testData/paired_end_test" \
   --umitools_bc_pattern "NNNN" \
   --umitools_bc_pattern2 "NNNN" \
-  --bbsplit_fasta_list testData/reference/bbsplit_fasta_list.txt \
-  --transcript_fasta testData/test_output/transcriptome.fasta \
-  --gtf testData/test_output/gene_annotation.gtf \
-  --bbsplit_index testData/test_output/bbsplit_index \
-  --ribo_database_manifest testData/reference/rrna-db-defaults.txt \
-  --salmon_index testData/test_output/salmon_index \
+  --bbsplit_fasta_list testData/minimal_test/reference/bbsplit_fasta_list.txt \
+  --transcript_fasta testData/minimal_test/reference/transcriptome.fasta \
+  --gtf testData/minimal_test/reference/gene_annotation.gtf \
+  --salmon_index testData/minimal_test/reference/salmon.tar.gz \
   --skip_trimming false \
-  --remove_ribo_rna true
-  # -profile docker \
-#   -resume
+  --trimmer fastp \
+  --remove_ribo_rna true \
+  -profile docker \
+  -resume
