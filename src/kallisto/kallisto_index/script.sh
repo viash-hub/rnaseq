e891ee514a93d5b97e020431e2fed9e75428b1fc
Nirmayi <ynirmayi@gmail.com>
2024-04-01 22:01:24 +0200

    fix info

--- src/kallisto/kallisto_index/script.sh
+++ src/kallisto/kallisto_index/script.sh
@@ -2,8 +2,7 @@
 
 set -eo pipefail
 
-kallisto \
-    index \
+kallisto index \
     ${par_pseudo_aligner_kmer_size:+-k $par_pseudo_aligner_kmer_size} \
     -i $par_kallisto_index \
     $par_transcriptome_fasta
