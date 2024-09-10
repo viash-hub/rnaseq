6ff8838298bed719bd04c349a6ec8933f706e991
Nirmayi <ynirmayi@gmail.com>
2024-09-10 14:21:11 +0200

    fix test

--- src/bedtools_genomecov/test.sh
+++ src/bedtools_genomecov/test.sh
@@ -5,18 +5,18 @@ echo ">>> Testing $meta_functionality_name"
 
 "$meta_executable" \
     --strandedness unstranded \
-    --bam $meta_resources_dir/test.paired_end.sorted.bam \
-    --bedgraph_forward forward.bedgraph \
-    --bedgraph_reverse reverse.bedgraph
+    --bam $meta_resources_dir/chr19.bam \
+    --bedgraph_forward chr19_forward.bedgraph \
+    --bedgraph_reverse chr19_reverse.bedgraph
 
 exit_code=$?
 [[ $exit_code != 0 ]] && echo "Non zero exit code: $exit_code" && exit 1
 
 # check whether output exists
-[ ! -f "forward.bedgraph" ] && echo "File 'forward.bedgraph' does not exist!" && exit 1
-[ ! -s "forward.bedgraph" ] && echo "File 'forward.bedgraph' is empty!" && exit 1
-[ ! -f "reverse.bedgraph" ] && echo "File 'reverse.bedgraph' does not exist!" && exit 1
-[ ! -s "reverse.bedgraph" ] && echo "File 'reverse.bedgraph' is empty!" && exit 1
+[ ! -f "chr19_forward.bedgraph" ] && echo "File 'chr19_forward.bedgraph' does not exist!" && exit 1
+[ ! -s "chr19_forward.bedgraph" ] && echo "File 'chr19_forward.bedgraph' is empty!" && exit 1
+[ ! -f "chr19_reverse.bedgraph" ] && echo "File 'chr19_reverse.bedgraph' does not exist!" && exit 1
+[ ! -s "chr19_reverse.bedgraph" ] && echo "File 'chr19_reverse.bedgraph' is empty!" && exit 1
 
 echo "All tests succeeded!"
 exit 0
