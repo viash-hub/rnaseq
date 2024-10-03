104ae62fa68d64918b78ccaf4474fa799806cec1
Toni Verbeiren <toni.verbeiren@gmail.com>
2024-10-03 17:45:26 +0200

    Update deseq2_qc for new assets location

--- src/deseq2_qc/test.sh
+++ src/deseq2_qc/test.sh
@@ -5,8 +5,6 @@ echo "> Running $meta_functionality_name"
 
 "$meta_executable" \
     --counts $meta_resources_dir/counts.tsv \
-    --pca_header_multiqc $meta_resources_dir/deseq2_pca_header.txt \
-    --clustering_header_multiqc $meta_resources_dir/deseq2_clustering_header.txt \
     --extra_args "--id_col 1 --sample_suffix '' --outprefix deseq2 --count_col 2" \
     --extra_args2 "test" \
     --deseq2_output "deseq2/" \
