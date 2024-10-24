14e8c7b1db0b9f40e0558b7597bc240005c4d77c
Nirmayi <ynirmayi@gmail.com>
2024-10-24 16:04:51 +0200

    move biotypes header file into multiqc_custom_biotype component

--- src/multiqc_custom_biotype/script.sh
+++ src/multiqc_custom_biotype/script.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-cut -f 1,7 $par_biocounts | tail -n +3 | cat $par_biotypes_header - >> $par_featurecounts_multiqc
+cut -f 1,7 $par_biocounts | tail -n +3 | cat "$meta_resources_dir/biotypes_header.txt" - >> $par_featurecounts_multiqc
 
 python3 "$meta_resources_dir/mqc_features_stat.py" \
     $par_featurecounts_multiqc \
